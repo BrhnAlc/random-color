e4082a4a8c817c443ceee2d737bf76057f58cbeb
burhan <alacaburhan122016@gmail.com>
2023-11-01 00:04:16 +0300

    random color

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [hex, setHex] = useState("#ffffff");
+
+function randomHex(){
+  const randomHex="#" + Math.floor(Math.random()*16777721).toString(16)
+  setHex(randomHex);
+}
+
   return (
     <div className="App">
+     <h1>{hex}</h1>
+     <button onClick={randomHex}>click for new Background</button> <hr />
+     <button onClick={()=>navigator.clipboard.writeText(hex)}>copy the hex value</button>
      
     </div>
   );
